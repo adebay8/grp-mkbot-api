29a8370ebfc4c4ed228539744f1f1af0dff7be22
Onuchukwu Adebayo <adebayop.o@gmail.com>
2023-04-24 20:14:15 +0100

    feat: updated config to work with google secret manager

--- mkbot/settings.py
+++ mkbot/settings.py
@@ -12,25 +12,42 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import io
 import environ
 from urllib.parse import urlparse
-
+from google.cloud import secretmanager
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load env variables
+
 env = environ.Env(DEBUG=(bool, False))
 env_file = os.path.join(BASE_DIR, ".env")
-env.read_env(env_file)
+
+if env("DEBUG"):
+    env.read_env(env_file)
+elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    # Pull secrets from Secret Manager
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+    client = secretmanager.SecretManagerServiceClient()
+    settings_name = os.environ.get("SETTINGS_NAME", "mkbot_django_settings")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+
+    env.read_env(io.StringIO(payload))
+else:
+    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-wu$46h*wrorl^of67rqe&ntfj^rc*2u=)9##+4t*4+pq80a3fp"
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = "RENDER" not in os.environ
 DEBUG = env("DEBUG")
 
 APPENGINE_URL = env("APPENGINE_URL", default=None)
@@ -42,49 +59,43 @@ if APPENGINE_URL:
 
     ALLOWED_HOSTS = [
         urlparse(APPENGINE_URL).netloc,
-        "https://mkbot-383411.nw.r.appspot.com",
     ]
     CSRF_TRUSTED_ORIGINS = [APPENGINE_URL]
     SECURE_SSL_REDIRECT = True
 else:
-    ALLOWED_HOSTS = [
-        '127.0.0.1',
-        "https://mkbot-383411.nw.r.appspot.com"
-    ]
+    ALLOWED_HOSTS = ["127.0.0.1", "https://mkbot-383411.nw.r.appspot.com"]
 
 if DEBUG:
     LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                "datefmt": "%d/%b/%Y %H:%M:%S",
+            },
+            "simple": {"format": "%(levelname)s %(message)s"},
         },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
+        "handlers": {
+            "file": {
+                "level": "DEBUG",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(BASE_DIR, "mkbot/debug.log"),
+                "formatter": "verbose",
+            },
         },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, "mkbot/debug.log"),
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
+        "loggers": {
+            "django": {
+                "handlers": ["file"],
+                "propagate": True,
+                "level": "DEBUG",
+            },
+            "MYAPP": {
+                "handlers": ["file"],
+                "level": "DEBUG",
+            },
         },
     }
-}
 
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
@@ -148,9 +159,7 @@ DATABASES = {
         "ENGINE": "djongo",
         "NAME": "mkbot",
         "ENFORCE_SCHEMA": False,
-        "CLIENT": {
-            "host": "mongodb+srv://darkskindeveloper:NO1jjbQWbvn7KAyL@demo-cluster.emvcvs1.mongodb.net/mkbot?retryWrites=true&w=majority"
-        },
+        "CLIENT": {"host": env("DATABASE_URL")},
     }
 }
 
